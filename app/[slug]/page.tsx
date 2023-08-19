4922e8f75462e84011e17957e7a98d65369765b8
srihari <hsri0007@gmail.com>
2023-08-19 14:00:59 +0530

    cfbfcbnnvb

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,16 +1,10 @@
-// 'use client'
-// import { useRouter } from 'next/router'
-
 
 async function getData(url:number) {
-  // const router = useRouter()
 
-  const res = await fetch(`http://localhost:3000/api?jello=${url}`)
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+  const res = await fetch(`${process.env.URL}/api?jello=${url}`)
+
  
   if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
     throw new Error('Failed to fetch data')
   }
  
@@ -19,10 +13,9 @@ async function getData(url:number) {
 
 export default async function Home({ params }:{params:{slug:number}}) {
   const data = await getData(params?.slug);
-console.log(params)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-asasasas
 {JSON.stringify(data)}
     </main>
   )
